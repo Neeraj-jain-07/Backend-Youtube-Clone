d41d82e767a4eb3402fc9ae2ccf0d5dea2c47c6f
Neeraj Jain <neerajrajjain12@gmail.com>
2024-02-19 17:09:38 +0530

    Database setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("i");
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/conn.js";
+const app = express();
+dotenv.config();
+connectDB();
+
+/*
+(async () => {
+  try {
+    // pending work of env 1 mongo_uri
+    await mongoose.connect(
+      `mongodb+srv://Neeraj:Neeraj%40123@cluster0.zlfviay.mongodb.net/${DB_NAME}`
+    );
+    app.on("error", (error) => {
+      console.log("Error :", error);
+      throw error;
+    });
+    // pending work of env 1 port
+    app.listen(4000, () => {
+      console.log(`App is listening on port 4000`);
+    });
+  } catch (error) {
+    console.log("Error", error);
+  }
+})();
+*/
